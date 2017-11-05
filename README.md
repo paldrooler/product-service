6f5e83c3bc2f14c60e89716b7c04c858472a9b1d
pal drooler <paldrooler@gmail.com>
2017-11-05 16:38:58 +1000

    added self starting server using supertest created a start-server.js element to handle server startup. borrowed heavily from strongloop documentation

---
+++ README.md
@@ -0,0 +1,11 @@
+# Product DB
+
+## overview
+
+A sample repository to implement an inventory management system as a learning tool.
+
+Uses MEAN stack and docker concepts to implement a microservices architecture.
+
+## known issues
+
+npm complains about REGEX DOS unless silenced
--- test/product.test.js
+++ test/product.test.js
@@ -1,19 +1,31 @@
 'use strict';
 
 var expect = require('chai').expect;
-var supertest = require('supertest');
-var api = supertest('http://localhost:3000/api/');
+var app = require('../server/server.js');
+var request = require('supertest');
+
+function json(verb, url) {
+  return request(app)[verb](url)
+    .set('Content-Type', 'application/json')
+    .set('Accept', 'application/json')
+    .expect('Content-Type', /json/);
+}
 
 describe('Product', function() {
+  before(function(done) {
+    require('./start-server');
+    done();
+  });
+
   it('should get all products', function(done) {
-    api.get('products')
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .end(function(err, res) {
+    json('get', '/api/products')
+      .expect(200, function(err, res) {
         if (err) {
           return done(err);
         }
         var products = res.body;
+
+        // we have pre-primed the file based DB with one entry
         expect(products.length).to.be.above(0);
         done();
       });

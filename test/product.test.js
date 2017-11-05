353c60d9e9628816299988770f9b44834d1cf2a5
pal drooler <paldrooler@gmail.com>
2017-11-05 12:57:14 +1000

    first commit

---
+++ test/product.test.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var expect = require('chai').expect;
+var supertest = require('supertest');
+var api = supertest("http://localhost:3000/api/");
+
+describe('Product', function(){
+    it('should get all products', function(done){
+        api.get('products')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(function(err, res){
+            if (err) {
+                return done(err);
+            }
+            var products = res.body;
+            expect(products.length).to.be.above(0);
+            done();
+        });
+    });
+});

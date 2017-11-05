1e8030b80af672b219fdca28de03801e60b87346
pal drooler <paldrooler@gmail.com>
2017-11-05 16:07:34 +1000

    added .nsprc to avoid nsp warning around debug lib added a custom test target to package.json fixed eslint errors

--- test/product.test.js
+++ test/product.test.js
@@ -2,20 +2,20 @@
 
 var expect = require('chai').expect;
 var supertest = require('supertest');
-var api = supertest("http://localhost:3000/api/");
+var api = supertest('http://localhost:3000/api/');
 
-describe('Product', function(){
-    it('should get all products', function(done){
-        api.get('products')
-        .expect('Content-Type', /json/)
-        .expect(200)
-        .end(function(err, res){
-            if (err) {
-                return done(err);
-            }
-            var products = res.body;
-            expect(products.length).to.be.above(0);
-            done();
-        });
-    });
+describe('Product', function() {
+  it('should get all products', function(done) {
+    api.get('products')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function(err, res) {
+        if (err) {
+          return done(err);
+        }
+        var products = res.body;
+        expect(products.length).to.be.above(0);
+        done();
+      });
+  });
 });
